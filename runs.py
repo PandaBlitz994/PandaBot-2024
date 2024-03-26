06deb8e7d4e815997ff0955e447df07ac233e6e6
PandaBlitz994 <pandablitz.994@gmail.com>
2024-03-26 16:43:37 +0200

    run3 progress

--- runs.py
+++ runs.py
@@ -12,8 +12,8 @@ right_motor = Motor(Port.B)
 left_arm = Motor(Port.D)
 right_arm = Motor(Port.C)
 chassis = DriveBase(left_motor, right_motor, 56, 114)
-right_color= ColorSensor(Port.A)
-left_color= ColorSensor(Port.E)
+right_color = ColorSensor(Port.A)
+left_color = ColorSensor(Port.E)
 chassis.use_gyro(True)
 
 
@@ -22,24 +22,26 @@ def follow_line_with_time(time, speed, kp, side, sensor):
     left_motor.reset_angle()
     if side == "left":
         kp *= -1
-    while (left_motor.angle() < distance):
+    while left_motor.angle() < distance:
         error = target - sensor.reflection()
         left_motor.dc(int(speed + error * kp))
         right_motor.dc(int(speed - error * kp))
     left_motor.stop()
     right_motor.stop()
 
+
 def follow_line(distance, speed, kp, side, sensor):
     target = 50
     left_motor.reset_angle()
     if side == "left":
         kp *= -1
-    while (left_motor.angle() < distance):
+    while left_motor.angle() < distance:
         error = target - sensor.reflection()
         left_motor.dc(int(speed + error * kp))
         right_motor.dc(int(speed - error * kp))
     left_motor.stop()
     right_motor.stop()
+
 
 def until_black(detect_sensor, speed, kp, side, follow_sensor):
     target = 50
@@ -52,29 +54,36 @@ def until_black(detect_sensor, speed, kp, side, follow_sensor):
     left_motor.stop()
     right_motor.stop()
 
+
 def until_black_no_follow(detect_sensor, speed):
     while detect_sensor.reflection() > 10:
-        chassis.drive(speed=speed,turn_rate=0)
+        chassis.drive(speed=speed, turn_rate=0)
     chassis.brake()
+
 
 def until_bezh(detect_sensor, speed):
     while not 45 < detect_sensor.reflection() < 50:
-        chassis.drive(speed=speed,turn_rate=0)
+        chassis.drive(speed=speed, turn_rate=0)
     chassis.brake()
-    
 
-        
+
 def run3():
     chassis.settings(straight_speed=700)
     chassis.straight(500)
     right_arm.run_angle(150, 135)
-    chassis.straight(-500)
+    chassis.straight(-250)
+    chassis.turn(20)
+    chassis.straight(100)
+    chassis.turn(10)
+
 
 def run2():
     chassis.settings(straight_speed=400)
     chassis.straight(800)
-    chassis.settings(straight_speed=700)
+    chassis.settings(straight_speed=699)
     chassis.straight(-1000)
+
+
 def run1():
     chassis.settings(straight_speed=150)
     chassis.straight(315)
@@ -88,8 +97,9 @@ def run1():
     chassis.straight(30)
     chassis.straight(distance=-650)
 
+
 def run4():
-    chassis.drive(300,0)
+    chassis.drive(300, 0)
     wait(2000)
     chassis.brake()
     chassis.turn(-10)
@@ -98,6 +108,7 @@ def run4():
     chassis.straight(20)
     chassis.settings(straight_speed=500)
     chassis.straight(-700)
+
 
 def run5():
     right_arm.run_time(-200, 1000, wait=False)
@@ -133,13 +144,15 @@ def run5():
     chassis.turn(-10)
     chassis.settings(straight_speed=800)
     chassis.straight(700)
+
+
 def run6():
     chassis.settings(straight_speed=250)
     chassis.straight(750)
     wait(1000)
     chassis.straight(-750)
 
-    
+
 def run6():
     chassis.settings(straight_speed=250)
     chassis.straight(700)
@@ -152,11 +165,10 @@ def run6():
     chassis.straight(-700)
 
 
-   
 def run8():
     chassis.settings(straight_speed=250)
-   
-    until_black_no_follow(right_color,200)
+
+    until_black_no_follow(right_color, 200)
     chassis.turn(10)
     chassis.straight(180)
     chassis.straight(-280)
@@ -182,11 +194,7 @@ def run7():
 
 
 def run9():
-        until_bezh(right_color, 200)
-
-
-
-
+    until_bezh(right_color, 200)
 
 
 def stats():
@@ -195,15 +203,7 @@ def stats():
     print("battery: ", precent, "%")
 
 
-
-
-
-
-
-
 selected = hub_menu(1, 2, 3, 4, 5, 6, 7, 8, 9, 99)
-
-
 
 
 if selected == 1:
@@ -226,4 +226,3 @@ elif selected == 99:
     stats()
 elif selected == 9:
     run9()
-
